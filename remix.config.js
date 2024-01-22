bc33b42be09c6e74c779cce0b0a08362308b1ff2
Brad Garropy <bradgarropy@gmail.com>
2024-01-22 15:21:18 -0600

    update remix config to use custom server.

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,11 @@ const config = {
     appDirectory: "src",
     ignoredRouteFiles: ["**/.*"],
     future: {},
+    server: "./server.ts",
+    serverBuildPath: "functions/[[path]].js",
+    serverConditions: ["workerd", "worker", "browser"],
     serverModuleFormat: "esm",
+    serverPlatform: "neutral",
 }
 
 export default config
